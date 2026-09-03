f6b52f1871e4298407bff134904f1fa46d9b55db
bcomnes <bcomnes@users.noreply.github.com>
2026-09-03 18:49:22 +0000

    0.0.1

--- version.go
+++ version.go
@@ -1,5 +1,5 @@
-// Package gobump stores release metadata for the go-bump action repository.
 package gobump
 
-// Version is the source release version managed by goversion.
-var Version = "dev"
+var (
+	Version = "0.0.1"
+)
